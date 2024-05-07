8128f1379f686bfdae05f51574d1cdce73deb4ea
rroyhere <rroyhere@gmail.com>
2024-05-06 21:08:24 -0500

    Update GreetingServerImpl.java
    server streaming and client streaming

--- grpc/src/main/java/greeting/server/GreetingServerImpl.java
+++ grpc/src/main/java/greeting/server/GreetingServerImpl.java
@@ -13,5 +13,39 @@ public class GreetingServerImpl extends GreetingServiceGrpc.GreetingServiceImplB
         respobnObserver.onNext(GreetingResponse.newBuilder().setResult("Hello " + request.getFirstName()).build());
         respobnObserver.onCompleted();
     }
-    
+
+    @Override
+    public void greetManyTimes(GreetingRequest request, StreamObserver<GreetingResponse> responseObserver) {
+        GreetingResponse response = GreetingResponse.newBuilder().setResult("Hello" + request.getFirstName()).build();
+
+        for (int i = 0; i < 10; ++i){
+            responseObserver.onNext(response);
+        }
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<GreetingRequest> longGreet(StreamObserver<GreetingResponse> responseObserver) {
+        StringBuilder sb = new StringBuilder();
+
+        return new StreamObserver<GreetingRequest>() {
+            @Override
+            public void onNext(GreetingRequest greetingRequest) {
+                sb.append("Hello ");
+                sb.append(greetingRequest.getFirstName());
+                sb.append("!\n");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                responseObserver.onError(throwable);
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(GreetingResponse.newBuilder().setResult(sb.toString()).build());
+                responseObserver.onCompleted();
+            }
+        };
+    }
 }
